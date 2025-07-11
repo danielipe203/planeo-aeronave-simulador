20ad08e8c3a26ff7ee6fac6908ac07f6e94209bb
danielipe203 <danielipe203@gmail.com>
2025-07-11 00:41:29 +0000

    se creo la clase FlightSimulator

--- src/flight_simulator.py
+++ src/flight_simulator.py
@@ -1,3 +1,11 @@
 import numpy as np
 from src.aircraft import Aircraft
 
+class FlightSimulator:
+    def __init__(self, aircraft: Aircraft, h0: float, v0: float, rho: float = 1.225):
+        self.aircraft = aircraft
+        self.h0 = h0  # Altitud inicial
+        self.v0 = v0  # Velocidad inicial
+        self.rho = rho  # Densidad del aire
+        self.g = 9.81  # Gravedad
+
