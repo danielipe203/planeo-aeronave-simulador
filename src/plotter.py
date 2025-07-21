0307928bacc183187ef49c6806e349b9ef6ed780
arenasjuanita <juanita.arenasgaviria05@gmail.com>
2025-07-21 01:10:54 +0000

    Feat: implementar módulo Plotter para visualización de trayectoria y velocidad

--- src/plotter.py
+++ src/plotter.py
@@ -1,1 +1,26 @@
-
+import matplotlib.pyplot as plt
+class Plotter:
+    def __init__(self):
+        pass
+    def plot_trajectory(self, distancias, alturas):
+        """Gráfica de la trayectoria (altura vs distancia)"""
+        plt.figure(figsize=(10,5))
+        plt.plot(distancias, alturas, color='blue', label='Trayectoria de Planeo')
+        plt.xlabel("Distancia horizontal (m)")
+        plt.ylabel("Altura (m)")
+        plt.title("Simulación de Trayectoria de Planeo")
+        plt.grid(True)
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+    def plot_velocity(self, tiempos, velocidades):
+        """Gráfica de velocidad vs tiempo"""
+        plt.figure(figsize=(10, 5))
+        plt.plot(tiempos, velocidades, color='green', label='Velocidad')
+        plt.xlabel("Tiempo (s)")
+        plt.ylabel("Velocidad (m/s)")
+        plt.title("Velocidad vs Tiempo durante el Planeo")
+        plt.grid(True)
+        plt.legend()
+        plt.tigjt_layout()
+        plt.show()
